3cea24fad41df307cf07901f9860be8bc0fa626b
Mathew Liew <mliew21396@gmail.com>
2015-10-09 21:56:35 -0500

    Finish kaminari pagination spike

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,9 @@
 class RestaurantsController < ApplicationController
 
+  def index
+    @restaurants = Restaurant.order("name").page(params[:page]).per(5)
+  end
+
   def show
     @restaurant = Restaurant.find(params[:id])
   end
