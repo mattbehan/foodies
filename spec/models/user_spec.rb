4c2653d5871db3403e5a6da3d8fc2d29388ab1dc
Harrison Klucher & Mathew Liew <pair+hklucher.mliew21396@devbootcamp.com>
2015-10-10 17:52:34 -0500

    add passing user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,5 +25,44 @@ describe User do
     end
   end
 
+  context "admin?" do
+    it "returns true if the user is an admin" do
+      user.role = "admin"
+      expect(user.admin?).to eq(true)
+    end
+  end
+
+  context "user?" do
+    it "returns true if the user is not an admin and is a user" do
+      user.role = "user"
+      expect(user.user?).to eq(true)
+    end
+  end
+
+  context "reviewer?" do
+    it "returns true if the user is a reviewer" do
+      user.role = "reviewer"
+      expect(user.reviewer?).to eq(true)
+    end
+  end
+
+  context "all_users" do
+    it "returns a collection of users" do
+      expect(User.all_users.count).to eq(User.where(role: "user").count)
+    end
+  end
+
+  context "all_admins" do
+    it "returns a collection of all admins" do
+      expect(User.all_admins.count).to eq(User.where(role: "admin").count)
+    end
+  end
+
+  context "all_reviewers" do
+    it "returns a collection of all reviewers" do
+      expect(User.all_reviewers.count).to eq(User.where(role: "reviewer").count)
+    end
+  end
+
 
 end
