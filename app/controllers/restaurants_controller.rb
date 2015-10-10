9e67c057408be4eea9165823d23bffcd38302964
Mathew Liew <mliew21396@gmail.com>
2015-10-09 22:21:32 -0500

    Finish multiple word search and pagination implementation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,14 @@
 class RestaurantsController < ApplicationController
 
   def index
-    @restaurants = Restaurant.order("name").page(params[:page]).per(5)
+    if params[:search]
+      @restaurants = Restaurant.search(params[:search]).order("name").page(params[:page]).per(5)
+
+      # @articles = Article.search(params[:search]).order("created_at DESC")
+    else
+      @restaurants = Restaurant.order("name").page(params[:page]).per(5)
+      # @articles = Article.order("created_at DESC")
+    end
   end
 
   def show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -16,4 +16,17 @@ class Restaurant < ActiveRecord::Base
   validates_inclusion_of :price_scale, :in => 1..5
   validates_inclusion_of :vegan_friendliness, :in => 1..5
   validates_format_of :zip, with: /\d{5}/
+
+  def self.search(query)
+    # where(:title, query) -> This would return an exact match of the query
+    # where("lower(name) LIKE ?", "%#{query.downcase}%")
+
+    if query
+      query_length = query.split.length
+      query_input = [(['lower(name) LIKE ?'] * query_length).join(' AND ')] + query.split.map { |name| "%#{name.downcase}%" }
+      where(query_input)
+    else
+      where(:all)
+    end
+  end
 end
