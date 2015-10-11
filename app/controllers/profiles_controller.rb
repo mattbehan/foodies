1eaee55be9f4992c6bc3c9600529a96a548354e7
Matt Behan <email": null>
2015-10-10 22:09:38 -0500

    completed profile new and create routes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,22 @@
 class ProfilesController < ApplicationController
 
 	before_filter :must_be_logged_in
-	before_filter :find_user
-	before_filter :find_profile
+	# before_filter :find_user
+	before_filter :find_profile, except: [:create]
 
 	def new
-		@profile =Profile.new
+		@profile = Profile.new
 	end
 
 	def create
+		# redirect_to :show unless current_user.profile = nil
+		@profile = Profile.new( profile_create_params.merge(user_id: current_user.id) )
+		if @profile.save
+			flash[:alert] = "Your profile has been created"
+			redirect_to user_path(current_user)
+		else
+			render :new
+		end
 	end
 
 	def show
@@ -20,4 +28,16 @@ class ProfilesController < ApplicationController
 
 	def update
 	end
+
+	protected
+
+	def profile_create_params
+    params.require(:profile).permit(:username, :full_name, :short_bio, :affiliation)
+  end
+
+	def profile_update_params
+    params.require(:profile).permit(:full_name, :short_bio, :affiliation)
+  end
+
+
 end
