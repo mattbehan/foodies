4e4780029609ed007bba19b1b7577c162c34c423
Harrison Klucher & Mathew Liew <pair+hklucher.mliew21396@devbootcamp.com>
2015-10-11 12:16:04 -0500

    begin ajaxing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,14 +7,14 @@ class CommentsController < ApplicationController
   #   @comment = Comment.new
   # end
 
+  respond_to :html, :js
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.find_by(id: params[:id])
     @comment = Comment.new(comment_params)
     @comment.review = @review
-
-    # Ajax later
+    # Ajax NOW
     if @comment.save
       redirect_to restaurant_review_path(@restaurant, @review)
     else
@@ -26,14 +26,14 @@ class CommentsController < ApplicationController
     prepare_vote  # Calls private method to set up vote for creation or update
     new_value = @vote.value + 1
     @vote.update_attributes(value: new_value)
-    redirect_to restaurant_review_path(@comment.review.restaurant, @comment.review)
+    redirect_to :back
   end
 
   def downvote
     prepare_vote
     new_value = @vote.value - 1
     @vote.update_attributes(value: new_value)
-    redirect_to restaurant_review_path(@comment.review.restaurant, @comment.review)
+    redirect_to :back
   end
 
   # def show
