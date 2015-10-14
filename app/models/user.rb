96818cd2dd2585463b7a4875ede88dbbd5977115
Harrison Klucher & Mathew Liew <pair+hklucher.mliew21396@devbootcamp.com>
2015-10-14 09:30:03 -0500

    add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -245,7 +245,7 @@ class User < ActiveRecord::Base
   private
 
   def reviewer_has_reviews?
-    self.role == "reviewer" && self.reviews.count > 0
+    self.reviewer? && self.reviews.count > 0
   end
 
 #   def welcome_message
