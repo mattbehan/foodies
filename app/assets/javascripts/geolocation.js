8929ef8f31f8a770dfaed157a8702bf9e38011b5
Mathew Liew <mliew21396@gmail.com>
2015-10-15 19:33:51 -0500

    Fix geolocation bug

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -1,34 +1,38 @@
-$(".search-form").on("submit", function(event){
-  event.preventDefault();
-  $this = this;
-  $('.search-bar').addClass('loadinggif');
+$(function() {
+  $(".search-form").on("submit", function(event){
+    event.preventDefault();
+    $this = this;
+    $('.search-bar').addClass('loadinggif');
 
-  var latitude, longitude;
-  navigator.geolocation.getCurrentPosition(success, fail,
-  {
-    enableHighAccuracy: true,
-    timeout: 10 * 1000 // 10 seconds
-  });
+    var latitude, longitude;
+    navigator.geolocation.getCurrentPosition(success, fail,
+    {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000 // 10 seconds
+    });
 
-  function success(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+    function success(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
 
-    var input1 =  $("<input>")
-                  .attr("type", "hidden")
-                  .attr("name", "lat_data")
-                  .attr("value",latitude);
-    var input2 =  $("<input>")
-                  .attr("type", "hidden")
-                  .attr("name", "long_data")
-                  .attr("value",longitude);
-    $('.search-form').append((input1));
-    $('.search-form').append((input2));
+      var input1 =  $("<input>")
+                    .attr("type", "hidden")
+                    .attr("name", "lat_data")
+                    .attr("value",latitude);
+      var input2 =  $("<input>")
+                    .attr("type", "hidden")
+                    .attr("name", "long_data")
+                    .attr("value",longitude);
+      $('.search-form').append((input1));
+      $('.search-form').append((input2));
+      console.log(latitude)
+      console.log(longitude)
 
-    $this.submit();
-  }
+      $this.submit();
+    }
 
-  function fail(msg) {
-    alert("Please enable your geolocation service in order to find directions.");
-  }
-})
+    function fail(msg) {
+      alert("Please enable your geolocation service in order to find directions.");
+    }
+  })
+});
