c2d0808985a6fd0fbd3edb1ee8d66cd6d0f7ea5b
Bill Leidy <wmleidy@gmail.com>
2015-10-15 23:30:05 -0500

    Fix bug in comments AJAX caused by hasty removal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,28 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+  // Comments
+  $(".bad-comment-warning a").on("click", function(){
+    $(this).parent().toggle();
+    $(this).parent().prev().show();
+  })
+
+  $(".comment-toggle").on("click", function(){
+    $(this).hide();
+    $(this).next().toggle();
+  })
+
+  $(".new-comment-form-toggle").on("click", function(){
+    $(this).hide();
+    $(this).next().toggle();
+  })
+  // Specialties
+  $(".specialties-container").on("click",".specialties-toggle", function(event){
+    event.preventDefault();
+    $(this).hide();
+    $(".rest-of-specialties").show();
+    $(".new-specialties-form").show();
+  })
+});
