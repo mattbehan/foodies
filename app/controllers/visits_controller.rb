e906fe5cca2281e21ede4f50254f3586cdc1dd22
Mathew Liew <mliew21396@gmail.com>
2015-10-19 18:53:15 -0500

    fix visits ajax

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -2,10 +2,13 @@ class VisitsController < ApplicationController
 
 	before_filter :must_be_logged_in
 
+	respond_to :html, :js, :json
+
 		def create
+		@restaurant = Restaurant.find(params[:restaurant_id])
 		@visit = Visit.find_or_initialize_by(visited_restaurant_id: params[:restaurant_id], visitor_id: current_user.id )
 		if @visit.new_record?
-			@visit.save
+			# @visit.save
 			if request.xhr?
 				return "Visit created"
 			end
